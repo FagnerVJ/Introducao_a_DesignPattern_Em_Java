b3aac0306bdc912b36b5fdd0c6b785f68832a95a
FagnerVJ <fagnerviana03@gmail.com>
2022-04-23 12:05:43 -0300

    Atualizando a branch

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/ef/1091859d02c3001c1619f50d3541eefd
@@ -0,0 +1,17 @@
+package br.com.alura.loja.orcamento;
+
+import java.math.BigDecimal;
+
+public class Orcamento {
+	private BigDecimal valor;
+
+	public Orcamento(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+		
+}
+//Aqui foi criado um valor como private e um get para usar esse valor em outras classes
---
+++ loja/src/br/com/alura/loja/descontos/CalculadoraDeDescontos.java
@@ -0,0 +1,22 @@
+package br.com.alura.loja.descontos;
+
+import java.math.BigDecimal;
+
+import br.com.alura.loja.orcamento.Orcamento;
+
+public class CalculadoraDeDescontos {
+	
+	public BigDecimal calcular(Orcamento orcamento) {
+		Desconto desconto = new DescontoParaOrcamentoComMaisDeCincoItens(
+				new DescontoParaOrcamentoComValorMaiorQueQuinhentos(
+						new SemDesconto()));
+		return desconto.calcular(orcamento);
+	}
+	
+}
+
+//Aqui foi enserido um padrão Chain of Responsibility
+//Verifiquei os tipos de descontos
+/* Classe nome = new classe (classe(classe(classe))) uma classe dentro da outra. 
+ */
+ 
